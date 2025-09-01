aabe6a27a610f7603825cfad8eeece1e7e570018
Thomas Scheelhardt <geminior@gmail.com>
2025-09-01 13:15:56 +0200

    auto pr title to branch name

--- DevConsole/Commands/CreatePullRequestCommand.cs
+++ DevConsole/Commands/CreatePullRequestCommand.cs
@@ -81,6 +81,7 @@ public sealed class CreatePullRequestCommand : DevConsoleCommand
             throw new UserActionException("Work item not found");
         }
 
+        title ??= branchName;
         if (string.IsNullOrWhiteSpace(title))
         {
             // title = GetWorkItem(workItemId.Value).Output?.Fields.Title;
