00de924387039eb4e1b58f71653fd7b61fa85614
Thomas Scheelhardt <geminior@gmail.com>
2025-09-04 14:38:48 +0200

    fix paths

--- DevConsole/Infrastructure/Services/Paths.cs
+++ DevConsole/Infrastructure/Services/Paths.cs
@@ -7,7 +7,7 @@ namespace DevConsole.Infrastructure.Services;
 
 public class Paths : IPaths
 {
-    private static readonly Lazy<string> MetaRootDirectory = new(() =>
+    private static readonly Lazy<string> ToolsRootDirectory = new(() =>
     {
         var directory = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ??
                                           throw new InvalidOperationException());
@@ -15,9 +15,9 @@ public class Paths : IPaths
         string metaRootDirectory = null!;
         while (directory is not null)
         {
-            if (directory.EnumerateFiles(".meta").Any())
+            if (directory.EnumerateFiles("DevConsole.sln").Any())
             {
-                metaRootDirectory = directory.FullName;
+                metaRootDirectory = directory.Parent?.FullName ?? throw new InvalidOperationException("Unable to find tools root dir");
                 break;
             }
 
@@ -27,5 +27,5 @@ public class Paths : IPaths
         return metaRootDirectory;
     });
 
-    public string DevConsoleFolder => Path.Combine(MetaRootDirectory.Value, "DevConsole");
+    public string DevConsoleFolder => Path.Combine(ToolsRootDirectory.Value, "DevConsole");
 }
